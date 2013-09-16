d351f3b903ac560420500d8da480d1f46ebf3bd7
BlackScorp <witalimik@web.de>
2013-09-16 20:51:27 +0200

    added a request

---
+++ src/OpenTribes/Core/City/Building/Queue/Add/Request.php
@@ -0,0 +1,13 @@
+<?php
+namespace OpenTribes\Core\City\Building\Queue\Add;
+
+use OpenTribes\Core\City;
+use OpenTribes\Core\City\Building;
+class Request{
+    protected $building;
+    protected $city;
+    public function __construct(){
+        
+    }
+    public function get
+}
