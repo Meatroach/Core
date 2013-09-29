4104d5b2632aa02b61f10bffe3beafa71706941e
BlackScorp <witalimik@web.de>
2013-09-29 19:52:55 +0200

    Updated Comments

--- src/OpenTribes/Core/Tile.php
+++ src/OpenTribes/Core/Tile.php
@@ -1,16 +1,61 @@
 <?php
+
+/**
+ * This file is part of the "Open Tribes" Core Module.
+ *
+ * @package    OpenTribes\Core
+ * @author     Witali Mik <mik@blackscorp.de>
+ * @copyright  (c) 2013 BlackScorp Games
+ * @license    For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace OpenTribes\Core;
 
-class Tile extends Entity{
- 
+/**
+ * Tile Entity
+ */
+class Tile extends Entity {
+
+    /**
+     * @var Bool $accessable
+     */
     protected $accessable;
-    
-    public function setAccessable($accessable){
+
+    /**
+     * @var String $name
+     */
+    protected $name;
+
+    /**
+     * @param String $name
+     * @return \OpenTribes\Core\Tile
+     */
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return String $name
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param Bool $accessable
+     * @return \OpenTribes\Core\Tile
+     */
+    public function setAccessable($accessable) {
         $this->accessable = (bool) $accessable;
         return $this;
     }
 
-    public function getAccessable(){
+    /**
+     * @return Bool $accessable
+     */
+    public function getAccessable() {
         return $this->accessable;
     }
+
 }
