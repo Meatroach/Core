a7e3c02584432fd0b76904074fa6e80e84b73837
BlackScorp <witalimik@web.de>
2013-09-29 20:51:48 +0200

    Updated Comments

--- src/OpenTribes/Core/City/Building.php
+++ src/OpenTribes/Core/City/Building.php
@@ -1,37 +1,85 @@
 <?php
 
+/**
+ * This file is part of the "Open Tribes" Core Module.
+ *
+ * @package    OpenTribes\Core
+ * @author     Witali Mik <mik@blackscorp.de>
+ * @copyright  (c) 2013 BlackScorp Games
+ * @license    For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace OpenTribes\Core\City;
 
 use OpenTribes\Core\Entity;
 use OpenTribes\Core\City;
 use OpenTribes\Core\Building as BaseBuilding;
+
+/**
+ * City Building relationship Entity
+ */
 class Building extends Entity {
 
+    /**
+     * @var Integer $level 
+     */
     protected $level;
+
+    /**
+     * @var \OpenTribes\Core\Building $building
+     */
     protected $building;
+
+    /**
+     * @var \OpenTribes\Core\City $city
+     */
     protected $city;
-    public function setLevel($level){
-        $this->level = (int)$level;
+
+    /**
+     * @param Integer $level
+     * @return \OpenTribes\Core\City\Building
+     */
+    public function setLevel($level) {
+        $this->level = (int) $level;
         return $this;
     }
-  
 
-    public function setBuilding(BaseBuilding $building){
+    /**
+     * @param \OpenTribes\Core\Building $building
+     * @return \OpenTribes\Core\City\Building
+     */
+    public function setBuilding(BaseBuilding $building) {
         $this->building = $building;
         return $this;
     }
-    public function setCity(City $city){
+
+    /**
+     * @param \OpenTribes\Core\City $city
+     * @return \OpenTribes\Core\City\Building
+     */
+    public function setCity(City $city) {
         $this->city = $city;
         return $this;
     }
-    public function getLevel(){
+
+    /**
+     * @return \Integer $level
+     */
+    public function getLevel() {
         return $this->level;
     }
 
-    public function getBuilding(){
+    /**
+     * @return \OpenTribes\Core\Building $building
+     */
+    public function getBuilding() {
         return $this->building;
     }
-    public function getCity(){
+
+    /**
+     * @return \OpenTribes\Core\City $city
+     */
+    public function getCity() {
         return $this->city;
     }
 
