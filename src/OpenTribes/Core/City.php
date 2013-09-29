5727a913248e7bc0b50f2078f4a1719ff9828e27
BlackScorp <witalimik@web.de>
2013-09-29 21:32:46 +0200

    Updated Comments in City

--- src/OpenTribes/Core/City.php
+++ src/OpenTribes/Core/City.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the "Open Tribes" Core Module.
  *
@@ -7,33 +8,54 @@
  * @copyright  (c) 2013 BlackScorp Games
  * @license    For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
+
 namespace OpenTribes\Core;
 
 /**
  * A basic City class
  */
 class City extends Entity {
-    
 
+    /**
+     * @var String $name
+     */
     protected $name;
 
+    /**
+     * @var \OpenTribes\Core\User $owner
+     */
     protected $owner;
-  
+
+    /**
+     * @param \OpenTribes\Core\User $user
+     * @return \OpenTribes\Core\City
+     */
     public function setOwner(User $user) {
         $this->owner = $user;
         return $this;
     }
 
-  
-  
+    /**
+     * @return \OpenTribes\Core\User $owner
+     */
     public function getOwner() {
         return $this->owner;
     }
-    public function setName($name){
+
+    /**
+     * @param String $name
+     * @return \OpenTribes\Core\City
+     */
+    public function setName($name) {
         $this->name = $name;
         return $this;
     }
-    public function getName(){
+
+    /**
+     * @return \String $name
+     */
+    public function getName() {
         return $this->name;
     }
+
 }
