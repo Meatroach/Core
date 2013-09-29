96df38e68bbd29dae733c19bce9e4cca551b92d3
BlackScorp <witalimik@web.de>
2013-09-29 18:52:06 +0200

    Updated Comments

--- src/OpenTribes/Core/User/Role.php
+++ src/OpenTribes/Core/User/Role.php
@@ -5,35 +5,25 @@ namespace OpenTribes\Core\User;
 use OpenTribes\Core\Entity;
 use OpenTribes\Core\Role;
 use OpenTribes\Core\User;
-class Roles extends Entity{
-    protected $roles = array();
+
+class Role extends Entity{
+    protected $role;
     protected $user;
     public function setUser(User $user){
         $this->user = $user;
         return $this;
     }
 
-    public function addRole(Role $role){
-        $this->roles[$role->getId()] = $role;
+    public function setRole(Role $role){
+        $this->roles = $role;
         return $this;
     }
-    public function removeRole(Role $role){
-        if(isset($this->roles[$role->getId()])) unset($this->roles[$role->getId()]);
-    }
-    public function getRoles(){
+
+    public function getRole(){
         return $this->roles;
     }
     public function getUser(){
         return $this->user;
     }
 
-    public function hasRole($name){
-        foreach($this->roles as $role){
-            if($role->getName() === $name) return true;
-        }
-        return false;
-    }
-    public function hasAnyRoles(array $roles){
-        return count(array_intersect($this->roles,$roles)) > 0;
-    }
 }
