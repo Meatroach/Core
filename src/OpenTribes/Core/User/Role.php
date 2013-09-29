dde0c25d1853a64037f60e802c7f31fa7aa30266
BlackScorp <witalimik@web.de>
2013-09-29 18:56:05 +0200

    Update Roles and userroles comments

--- src/OpenTribes/Core/User/Role.php
+++ src/OpenTribes/Core/User/Role.php
@@ -1,27 +1,54 @@
 <?php
-
+/**
+ * This file is part of the "Open Tribes" Core Module.
+ *
+ * @package    OpenTribes\Core
+ * @author     Witali Mik <mik@blackscorp.de>
+ * @copyright  (c) 2013 BlackScorp Games
+ * @license    For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
 namespace OpenTribes\Core\User;
 
 use OpenTribes\Core\Entity;
 use OpenTribes\Core\Role;
 use OpenTribes\Core\User;
-
+/**
+ * User Role relationship Entity
+ */
 class Role extends Entity{
+    /**
+     * @var \OpenTribes\Core\Role $role
+     */
     protected $role;
+    /**
+     * @var \OpenTribes\Core\User $user 
+     */
     protected $user;
+    /**
+     * @param \OpenTribes\Core\User $user
+     * @return \OpenTribes\Core\User\Role
+     */
     public function setUser(User $user){
         $this->user = $user;
         return $this;
     }
-
+    /**
+     * @param \OpenTribes\Core\Role $role
+     * @return \OpenTribes\Core\User\Role
+     */
     public function setRole(Role $role){
-        $this->roles = $role;
+        $this->role = $role;
         return $this;
     }
-
+    /**
+     * @return \OpenTribes\Core\Role $role
+     */
     public function getRole(){
-        return $this->roles;
+        return $this->role;
     }
+    /**
+     * @return \OpenTribes\Core\User $role
+     */
     public function getUser(){
         return $this->user;
     }
