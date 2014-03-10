c6178b62ff83eb185add9bad9c9827c38fad8d87
BlackScorp <witalimik@web.de>
2014-03-10 09:03:26 +0100

    added php 5.3 password hash

--- app/Service/PasswordHasher.php
+++ app/Service/PasswordHasher.php
@@ -1,19 +1,32 @@
 <?php
 
 namespace OpenTribes\Core\Service;
+
 use OpenTribes\Core\Domain\Service\PasswordHasher as PasswordHasherInterface;
+
 /**
  * Description of PasswordHasher
  *
  * @author BlackScorp<witalimik@web.de>
  */
-class PasswordHasher implements PasswordHasherInterface{
+class PasswordHasher implements PasswordHasherInterface {
+
     public function hash($rawPassword) {
-        return password_hash($rawPassword, PASSWORD_DEFAULT);
+        if (function_exists('password_hash')) {
+            return password_hash($rawPassword, PASSWORD_DEFAULT);
+        }
+        $salt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
+        $salt = base64_encode($salt);
+        $salt = str_replace('+', '.', $salt);
+        $hash = crypt($rawPassword, '$2y$10$' . $salt . '$');
+        return $hash;
     }
 
     public function verify($hash, $rawPassword) {
-        return password_verify($rawPassword, $hash);
+        if (function_exists('password_verify')) {
+            return password_verify($rawPassword, $hash);
+        }
+        return crypt($rawPassword, $hash) === $hash;
     }
 
 }
