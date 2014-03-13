5ce98a09ec442d11bb45468c4d82d744b465efaf
BlackScorp <witalimik@web.de>
2014-03-13 14:36:08 +0100

    removed dead code

--- cli/index.php
+++ cli/index.php
@@ -10,14 +10,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 $console = new Application;
 $console->register('install-shema')
         ->setDescription('install the database')
-        ->setCode(function (InputInterface $input, OutputInterface $output) use($app) {
+        ->setCode(function () use($app) {
             $shema = new Shema($app['db']);
             $shema->installShema();
         });
 
 $console->register('install-roles')
         ->setDescription('install the database')
-        ->setCode(function (InputInterface $input, OutputInterface $output) use($app) {
+        ->setCode(function () use($app) {
             $shema = new Shema($app['db']);
             $shema->createRoles();
         });
