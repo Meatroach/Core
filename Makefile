90524597b09474d07cc12ede5128094d837d44c3
BlackScorp <witalimik@web.de>
2014-03-16 20:34:21 +0100

    added develop/production/test installation

--- Makefile
+++ Makefile
@@ -10,12 +10,19 @@ install-test:
 	php cli/index.php install-shema
 	php cli/index.php install-roles
 
-install:
+install-dev:
 	composer self-update
 	composer install --no-progress
-	php cli/index.php install-shema
-	php cli/index.php install-roles
+	mkdir storage
+	touch storage/ot.db
+	php cli/index.php install-shema develop
+	php cli/index.php install-roles develop
 
+install-production:
+	composer self-update
+	composer install --no-progress
+	php cli/index.php install-shema production
+	php cli/index.php install-roles production
 update:
 	composer update
 	php cli/index.php install-shema
--- cli/index.php
+++ cli/index.php
@@ -1,21 +1,31 @@
 <?php
 
-require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use OpenTribes\Core\Silex\Shema;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 $console = new Application;
+
 $console->register('install-shema')
         ->setDescription('install the database')
-        ->setCode(function () use($app) {
+        ->addArgument('env', InputArgument::OPTIONAL, 'Sets the enviroment for shema ', 'test')
+        ->setCode(function (InputInterface $input) {
+
+            $env   = $input->getArgument('env');
+            $app   = require __DIR__ . '/../bootstrap.php';
             $shema = new Shema($app['db']);
             $shema->installShema();
         });
 
 $console->register('install-roles')
-        ->setDescription('install the database')
-        ->setCode(function () use($app) {
+        ->setDescription('install roles')
+        ->addArgument('env', InputArgument::OPTIONAL, 'Sets the enviroment for shema ', 'test')
+        ->setCode(function (InputInterface $input) {
+            $env   = $input->getArgument('env');
+            $app   = require __DIR__ . '/../bootstrap.php';
             $shema = new Shema($app['db']);
             $shema->createRoles();
         });
