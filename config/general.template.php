b2d28fe2a6bb2e6f5672a6c61aa79e2def4c51a9
BlackScorp <witalimik@web.de>
2014-03-26 15:22:33 +0100

    fixed paths in config

--- config/general.template.php
+++ config/general.template.php
@@ -2,7 +2,7 @@
 
 return array(
     'debug'         => true,
-    'mustache.path' => realpath(__DIR__ . '/../../templates'),
-    'mustache.assets' => array(realpath(__DIR__ . '/../../templates/assets')),
+    'mustache.path' => realpath(__DIR__ . '/../templates'),
+    'mustache.assets' => array(realpath(__DIR__ . '/../templates/assets')),
     'activationCodeLength' => 8
 );
