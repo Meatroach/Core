09627ce2a08ba181bd65633499510cfd7a4a43da
BlackScorp <witalimik@web.de>
2014-04-05 19:05:35 +0200

    changed test

--- tests/Mock/Repository/CityTest.php
+++ tests/Mock/Repository/CityTest.php
@@ -15,13 +15,13 @@ class CityTest extends \PHPUnit_Framework_TestCase {
         $user = $this->userRepository->create($userId, 'TestUser', '123456', 'test@test.de');
                 
         $cityId = $this->cityRepository->getUniqueId();
-        $city = $this->cityRepository->create($cityId, 'TestCity', $user, "0", "0");
+        $city = $this->cityRepository->create($cityId, 'TestCity', $user, 0, 0);
         $this->cityRepository->add($city);
     }
     public function testCityExistsAtLocation(){
-        $this->assertTrue($this->cityRepository->cityExistsAt("0", "0"));
+        $this->assertTrue($this->cityRepository->cityExistsAt(0, 0));
     }
     public function testCityNotExistsAtLocation(){
-        $this->assertFalse($this->cityRepository->cityExistsAt("1", "0"));
+        $this->assertFalse($this->cityRepository->cityExistsAt(1, 0));
     }
 }
