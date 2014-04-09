48b561aadec78416e9ec5f6e390998b53a156559
Ma27 <ma27-se@hotmail.com>
2014-04-09 23:04:41 +0200

    Remove ServiceProviderInterface

--- silex/providers/CSRFTokenHasher.php
+++ silex/providers/CSRFTokenHasher.php
@@ -1,20 +1,35 @@
 <?php
 namespace Opentribes\Core\Silex\providers;
 
-use OpenTribes\Core\Silex\Service\PasswordHasher;
-use Silex\ServiceProviderInterface;
-use Silex\Application;
+use OpenTribes\Core\Service\PasswordHasher as PasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class CSRFTokenHasher implements ServiceProviderInterface {
-    public function register(Application $app) {
-        $hasher = new PasswordHasher();
-        $session = new Session();
-        
-        $id = $session->getId();
-        $app['csrf_token'] = $hasher->hash($id);
+class CSRFTokenHasher implements PasswordHasherInterface {
+    private $session;
+    
+    public function __construct(Session $session) {
+        $this->session = &$session;
     }
     
-    public function boot(Application $app) {
+    public function hash($rawPassword) {
+        $token = $this->session->getId();
+        if (function_exists('password_hash')) {
+            return password_hash($token , PASSWORD_DEFAULT);
+        }
+        
+        $salt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
+        $salt = base64_encode($salt);
+        $salt = str_replace('+', '.', $salt);
+        $hash = crypt($token , '$2y$10$' . $salt . '$');
+        return $hash;
+    }
+    
+    public function verify($hash, $rawPassword) {
+        $token = $this->session->getId();
+        if (function_exists('password_verify')) {
+            return password_verify($expectedToken, $token );
+        }
+        
+        return crypt($expectedToken, $token ) === $token ;
     }
 }
