403237bebd991cbb6a323aa2375e49c5cb00d9b5
Ma27 <ma27-se@hotmail.com>
2014-05-14 23:06:10 +0200

    Added account provider

--- silex/Module.php
+++ silex/Module.php
@@ -28,6 +28,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use OpenTribes\Core\Silex\Provider\Assets as AssetsController;
+use OpenTribes\Core\Silex\Provider\Account as AccountController;
 
 /**
  * Description of Module
@@ -140,7 +141,7 @@ class Module implements ServiceProviderInterface {
             }
         });
         $app->mount('/assets', new AssetsController());
-        $app->mount('/account', $this->getAccountRoutes($app));
+        $app->mount('/account', new AccountRoutes());
         $app->mount('/game', $this->getGameRoutes($app));
         $module = $this;
         $app->on(KernelEvents::VIEW, function($event) use($app, $module) {
@@ -279,91 +280,4 @@ class Module implements ServiceProviderInterface {
         });
         return $game;
     }
-
-    /**
-     * @param Application $app
-     * @return ControllerCollection
-     */
-    private function getAssetsRoutes(Application &$app) {
-        $assets = $app['controllers_factory'];
-
-        $assets->assert('file', '.+');
-        $assets->get('{type}/{file}', Controller::ASSETS . ':load');
-
-        return $assets;
-    }
-
-    /**
-     * 
-     * @param Application $app
-     * @return ControllerCollection
-     */
-    private function getAccountRoutes(Application &$app) {
-        $account = $app['controllers_factory'];
-        $account->get('/logout', function()use($app) {
-            $baseUrl = $app['mustache.options']['helpers']['baseUrl'];
-            $app['session']->remove('username');
-
-            return new RedirectResponse($baseUrl);
-        });
-        $account->post('/login', Controller::ACCOUNT . ':loginAction')
-                ->value(RouteValue::TEMPLATE, 'pages/landing')
-                ->value(RouteValue::SUCCESS_HANDLER, function($appResponse) use ($app) {
-                    $baseUrl = $app['mustache.options']['helpers']['baseUrl'];
-                    $app['session']->set('username', $appResponse->username);
-
-                    return new RedirectResponse($baseUrl);
-                })
-                ->value(RouteValue::SUB_REQUESTS, array(
-                    array(
-                        'url'    => '/',
-                        'method' => 'GET',
-                        'param'  => array())
-        ));
-        $account->get('/registration_successfull', function() use($app) {
-            $baseUrl = $app['mustache.options']['helpers']['baseUrl'];
-            return new RedirectResponse($baseUrl);
-        })->value(RouteValue::TEMPLATE, 'pages/registration_successfull');
-
-        $account->match('/create', Controller::ACCOUNT . ':createAction')
-                ->method('GET|POST')
-                ->value(RouteValue::SUCCESS_HANDLER, function($appResponse) use ($app) {
-                    $baseUrl = $app['mustache.options']['helpers']['baseUrl'];
-                    $request = $app['request'];
-
-                    $appResponse->url = $request->getHttpHost();
-
-                    $htmlBody = $app['mustache']->render('mails/html/register', $appResponse);
-                    $textBody = $app['mustache']->render('mails/text/register', $appResponse);
-                    $message  = Swift_Message::newInstance($app['subjects']['registration'])
-                            ->setFrom($app['fromMails']['registration'])
-                            ->setTo($appResponse->email)
-                            ->setBody($htmlBody, 'text/html')
-                            ->addPart($textBody, 'text/plain');
-
-                    if ($app['mailer']->send($message)) {
-                        $target = $baseUrl . 'account/registration_successfull';
-                    } else {
-                        $target = $baseUrl . 'account/registration_failed';
-                    }
-                    return new RedirectResponse($target);
-                })
-                ->value(RouteValue::TEMPLATE, 'pages/registration');
-
-
-        $account->match('/activate', Controller::ACCOUNT . ':activateAction')
-                ->method('GET|POST')
-                ->value(RouteValue::TEMPLATE, 'pages/activation');
-
-        $account->get('/activate/{username}/{activationKey}', Controller::ACCOUNT . ':activateAction')
-                ->value(RouteValue::TEMPLATE, 'pages/activation');
-
-        $account->after(function() use($app) {
-
-            return $app[Controller::ACCOUNT]->after();
-        });
-
-        return $account;
-    }
-
 }
