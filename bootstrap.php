ad1c5da9e3b627ed4921d533a4957a9be00d7de5
BlackScorp <witalimik@web.de>
2015-03-21 16:58:14 +0100

    enabled error reporting

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+error_reporting(-1);
+ini_set('display_errors',1);
 $app = new \Silex\Application();
 
 $app->register(new \OpenTribes\Core\Silex\Module());
